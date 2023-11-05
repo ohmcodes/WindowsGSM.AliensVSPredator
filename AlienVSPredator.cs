ef000bebdaf0442574d6812b1d4788f640e96d6f
Ohmcodes <ohmcodes@gmail.com>
2023-11-05 15:27:50 -0800

    final v1

---
+++ AlienVSPredator.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Engine;
+using WindowsGSM.GameServer.Query;
+using System.Collections.Generic;
+
+namespace WindowsGSM.Plugins
+{
+    public class AlienVSPredator : SteamCMDAgent
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.AlienVSPredator", // WindowsGSM.XXXX
+            author = "ohmcodes",
+            description = "WindowsGSM plugin for supporting Alien vs Predator Dedicated Server",
+            version = "1.0",
+            url = "https://github.com/ohmcodes/WindowsGSM.AlienVSPredator", // Github repository link (Best practice)
+            color = "#1E8449" // Color Hex
+        };
+
+        // - Standard Constructor and properties
+        public AlienVSPredator(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData;
+        public string Error, Notice;
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => false;
+        public override string AppId => "34120"; /* taken via https://steamdb.info/app/34120/info/ */
+
+        // - Game server Fixed variables
+        public override string StartPath => "AvP_CLI.exe"; // Game server start path
+        public string FullName = "Alien vs Predator Dedicated Server"; // Game server FullName
+        public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
+        public int PortIncrements = 0; // This tells WindowsGSM how many ports should skip after installation
+        public object QueryMethod = new UT3(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+
+        // - Game server default values
+        public string ServerName = "wgsm_avp_dedicated";
+        public string Defaultmap = "Jungle DM t1=15 s1=20"; // Original (MapName)
+        public string Maxplayers = "10"; // WGSM reads this as string but originally it is number or int (MaxPlayers)
+        public string Port = "27015"; // WGSM reads this as string but originally it is number or int
+        public string QueryPort = "27016"; // WGSM reads this as string but originally it is number or int (SteamQueryPort)
+        public string Additional = string.Empty;
+
+
+        private Dictionary<string, string> configData = new Dictionary<string, string>();
+
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG()
+        {
+            createConfigFile();
+        }
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+            if (!File.Exists(shipExePath))
+            {
+                Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
+                return null;
+            }
+
+            string param = string.Empty;
+
+            // Modify CFG before start
+            createConfigFile();
+
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    FileName = shipExePath,
+                    Arguments = param.ToString(),
+                    WindowStyle = ProcessWindowStyle.Minimized,
+                    UseShellExecute = false
+                },
+                EnableRaisingEvents = true
+            };
+
+            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+            if (AllowsEmbedConsole)
+            {
+                p.StartInfo.CreateNoWindow = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                var serverConsole = new ServerConsole(_serverData.ServerID);
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+            }
+
+            // Start Process
+            try
+            {
+                p.Start();
+                if (AllowsEmbedConsole)
+                {
+                    p.BeginOutputReadLine();
+                    p.BeginErrorReadLine();
+                }
+
+                return p;
+            }
+            catch (Exception e)
+            {
+                Error = e.Message;
+                return null; // return null if fail to start
+            }
+        }
+
+        // - Stop server function
+        public async Task Stop(Process p)
+        {
+            await Task.Run(() =>
+            {
+                Functions.ServerConsole.SendMessageToMainWindow(p.MainWindowHandle, "quit");
+            });
+            await Task.Delay(20000);
+        }
+
+        // - Update server function
+        public async Task<Process> Update(bool validate = false, string custom = null)
+        {
+            var (p, error) = await Installer.SteamCMD.UpdateEx(serverData.ServerID, AppId, validate, custom: custom, loginAnonymous: loginAnonymous);
+            Error = error;
+            await Task.Run(() => { p.WaitForExit(); });
+            return p;
+        }
+
+        public bool IsInstallValid()
+        {
+            return File.Exists(Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath));
+        }
+
+        public bool IsImportValid(string path)
+        {
+            string exePath = Path.Combine(path, "PackageInfo.bin");
+            Error = $"Invalid Path! Fail to find {Path.GetFileName(exePath)}";
+            return File.Exists(exePath);
+        }
+
+        public string GetLocalBuild()
+        {
+            var steamCMD = new Installer.SteamCMD();
+            return steamCMD.GetLocalBuild(_serverData.ServerID, AppId);
+        }
+
+        public async Task<string> GetRemoteBuild()
+        {
+            var steamCMD = new Installer.SteamCMD();
+            return await steamCMD.GetRemoteBuild(AppId);
+        }
+
+        private void createConfigFile()
+        {
+            // Define the path to the configuration file
+            string filePath = Path.Combine(ServerPath.GetServersServerFiles(_serverData.ServerID), "default.cfg");
+
+            Trace.WriteLine(filePath);
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            // Add some key-value pairs to the dictionary
+            configData["servername"] = $"\"{_serverData.ServerName}\"";
+            configData["maxplayers"] = _serverData.ServerMaxPlayer;
+            configData["motd"] = "\"Another great plugins from WGSM written by ohmcodes\"";
+            configData["authport"] = "27017";
+            configData["gameport"] = _serverData.ServerPort;
+            configData["lobbyport"] = _serverData.ServerQueryPort;
+            configData["updateport"] = "27018";
+            configData["addmap"] = _serverData.ServerMap;
+            configData["predatorratio"] = "1";
+            configData["marineratio"] = "2";
+            configData["alienratio"] = "3";
+            configData["speciesbalancing"] = "1";
+            configData["teambalancing"] = "1";
+            configData["friendlyfire"] = "1";
+            configData["autostartonallready"] = "1";
+            configData["autostarttime"] = "60";
+            configData["host"] = string.Empty;
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (var kvp in configData)
+                    {
+                        // Write each key-value pair as "key value" to the file
+                        writer.WriteLine($"{kvp.Key} {kvp.Value}");
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"An error occurred while writing the configuration file: {ex.Message}");
+            }
+        }
+    }
+}
